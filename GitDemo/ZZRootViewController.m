b8a298a12390cb3a6e97ed2455084cc7faa3e925
zhu zhi <zhuzhi2013@gmail.com>
2014-04-22 23:39:21 +0800

    add test code

--- GitDemo/ZZRootViewController.m
+++ GitDemo/ZZRootViewController.m
@@ -45,6 +45,8 @@
 
     // Add the page view controller's gesture recognizers to the book view controller's view so that the gestures are started more easily.
     self.view.gestureRecognizers = self.pageViewController.gestureRecognizers;
+    
+    // test git reset
 }
 
 - (void)didReceiveMemoryWarning
